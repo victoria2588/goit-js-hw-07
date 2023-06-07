6f16daacbc2269f89b10434cded173986ebd1459
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-07 19:11:56 +0300

    gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+console.log(listEl);
+
+const itemEl = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image"
+    src="${preview}" data-source="${original} "alt="${description}"
+    ></a></li>`
+);
+
+listEl.insertAdjacentHTML("beforeend", itemEl.join(""));
