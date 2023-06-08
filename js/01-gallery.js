f1b9071d2fd91036f8158e3e2a0c8eacb270f283
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-08 13:33:25 +0300

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,13 +4,32 @@ import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
 
 const listEl = document.querySelector(".gallery");
-console.log(listEl);
+const itemsEl = createGalleryItems(galleryItems);
+listEl.insertAdjacentHTML("beforeend", itemsEl);
 
-const itemEl = galleryItems.map(
-  ({ preview, original, description }) =>
-    `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image"
-    src="${preview}" data-source="${original} "alt="${description}"
-    ></a></li>`
-);
+function createGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+     <img class="gallery__image"
+    src="${preview}" data-source="${original}" alt="${description}"
+    />
+    </a>
+    </li>`;
+    })
+    .join("");
+}
 
-listEl.insertAdjacentHTML("beforeend", itemEl.join(""));
+listEl.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "BUTTON") {
+    const instance = basicLightbox.create(`
+    <img src='${event.target.dataset.source}' width="800" height="600">
+`);
+    instance.show();
+    return;
+  }
+}
