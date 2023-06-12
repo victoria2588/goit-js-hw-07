ccdad964aff0041c8985b5fbd7a7dfdca93d69e6
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-12 20:22:44 +0300

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+const itemsEl = createGalleryItems(galleryItems);
+listEl.insertAdjacentHTML("beforeend", itemsEl);
+
+function createGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+     <img class="gallery__image"
+    src="${preview}" alt="${description}" title="${description}"
+    />
+    </a>
+    </li>`;
+    })
+    .join("");
+}
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+  captionData: "title",
+});
